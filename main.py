fba75edb2b6f391ee4d87f4400206f9bd149d5b4
Tara Robinson <robinsonharris@gmail.com>
2016-09-12 18:58:35 -0500

    Adding final changes

--- main.py
+++ main.py
@@ -15,11 +15,68 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import cgi
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
 
 class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Blog(db.Model):#this allows us to create entity/table
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)#set created to be current time right now
+
+class MainBlogPage(MainHandler):
     def get(self):
-        self.response.write('Hello world!')
+        blogs = db.GqlQuery("SELECT * FROM Blog "
+                "ORDER BY created DESC LIMIT 5")
+
+        self.render("viewpost.html", blogs = blogs)
+
+class NewBlogPost(MainHandler):
+    def get(self):
+        self.render("newpost.html", title = "", body = "", error = "")
+
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            b = Blog(title = title, body = body)
+            key = b.put()
+            _id = key.id()#gives id of just created blog post
+            self.redirect("/blog/"+str(_id))
+        else:
+            error = "We need both a title and some content!"
+            self.render("newpost.html", title = title, body = body, error = error)
+
+class ViewPostHandler(MainHandler):
+    def get(self, id):
+        b = Blog.get_by_id(int(id))
+        if b is not None:
+            self.render("viewsinglepost.html", blog = b)
+        else:
+            error = "No post with that ID exists."
+            self.response.write(error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    webapp2.Route('/blog', handler =MainBlogPage),
+    webapp2.Route('/newpost', handler =NewBlogPost),
+    webapp2.Route('/blog/<id:\d+>', handler =ViewPostHandler)
 ], debug=True)
